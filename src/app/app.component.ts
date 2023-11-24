c6e72e386433536d64b23a8c4d0f0ba0033f07d1
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2023-11-24 19:03:39 +0330

    make autocomplete for symbol search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScreenerService } from './services/screener.service';
+import { Observable, of, OperatorFunction } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, map, tap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[ScreenerService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  model: any;
+	searching = false;
+	searchFailed = false;
   title = 'screener';
+  constructor(
+
+    private service: ScreenerService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      tap(() => (this.searching = true)),
+      switchMap((term) =>
+        this.service.searchSymbol(term)  
+        .pipe(
+          tap(() => (this.searchFailed = false)),
+          catchError(() => {
+            this.searchFailed = true;
+            return of([]);
+          }),
+        ),
+      ),
+      tap(() => (this.searching = false)),
+    );
+
 }
---
+++ src/app/services/screener.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, Observable, of, OperatorFunction } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ScreenerService {
+
+  constructor(private http: HttpClient) {
+
+  }
+  searchSymbol(term: string) {
+    if (term === '') {
+      return of([]);
+    }
+
+    return this.http
+    .get<[any, string[]]>(`https://endpoint.stockscreeners.ir/symbols?Filter=${term}`)
+    .pipe(map((response: any[]) => response[1]));
+  }
+}
