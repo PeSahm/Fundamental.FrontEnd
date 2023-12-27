99cab6edf47b35a7edf0fa3442a127ca6a069be1
Amir Mehrizi <a.mehrizi@PIKAD.COM>
2023-12-27 12:37:34 +0330

    change ux design of the app.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  encapsulation: ViewEncapsulation.None // Use None, Emulated, or ShadowDom
+
 })
 export class SidebarComponent {
+  isSearchBarOpen = true;
+  toggleSearchFilter(el: ElementRef) {
+    this.isSearchBarOpen = !this.isSearchBarOpen;
+  }
 
 }
