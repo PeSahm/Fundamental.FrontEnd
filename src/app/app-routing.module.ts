bee2b0d660659785d45c7d299c0b8a6c5f1ecdec
unknown <s.a.mehrizi1989@gmail.com>
2025-01-18 21:17:53 +0330

    remove two url form the sidebar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,9 +15,9 @@ import { SymbolShareHoldersComponent } from './pages/symbol-share-holders/symbol
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, data: { title: 'خانه' } },
-  { path: 'financial-report', component: FinancialReportComponent, data: { title: ' ثبت صورت های مالی ' } },
+  // { path: 'financial-report', component: FinancialReportComponent, data: { title: ' ثبت صورت های مالی ' } },
   { path: 'sales-report', component: SalesReportComponent, data: { title: ' ثبت فروش های ماهانه ' } },
-  { path: 'get-financial-report', component: GetFinancialReportComponent, data: { title: ' گزارش صورت های مالی ' } },
+  // { path: 'get-financial-report', component: GetFinancialReportComponent, data: { title: ' گزارش صورت های مالی ' } },
   { path: 'get-sales-report', component: GetSalesReportComponent, data: { title: ' گزارش فروش های ماهانه ' } },
   {
     path: 'manufacturing/balance-sheet', component: ManufacturingBalanceSheetComponent, data: { title: '  ثبت صورت وضعیت مالی تولیدی ' }
