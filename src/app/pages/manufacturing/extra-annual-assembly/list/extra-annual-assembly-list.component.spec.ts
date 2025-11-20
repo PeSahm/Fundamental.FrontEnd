65b46ae1c66fd7c63d089109ec357cc172ed8f70
Mostafa <mostafa.esmaeili8@gmail.com>
2025-11-20 14:01:36 +0330

    #7 Fix list component test dependencies
    Updated ExtraAnnualAssemblyListComponent test to include all required dependencies:
    - FormsModule for ngModel binding
    - NgxMaskDirective and NgxMaskPipe for input masking
    - SearchFilterComponent, SymbolSearchComponent, TableComponent declarations
    - provideNgxMask() provider
    
    This ensures the component template can render properly during testing.

--- src/app/pages/manufacturing/extra-annual-assembly/list/extra-annual-assembly-list.component.spec.ts
+++ src/app/pages/manufacturing/extra-annual-assembly/list/extra-annual-assembly-list.component.spec.ts
@@ -1,8 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 import { ExtraAnnualAssemblyListComponent } from './extra-annual-assembly-list.component';
 import { ExtraAnnualAssemblyService } from 'src/app/services/extra-annual-assembly.service';
+import { SearchFilterComponent } from 'src/app/components/search-filter/search-filter.component';
+import { SymbolSearchComponent } from 'src/app/components/symbol-search/symbol-search.component';
+import { TableComponent } from 'src/app/components/table/table.component';
 
 describe('ExtraAnnualAssemblyListComponent', () => {
   let component: ExtraAnnualAssemblyListComponent;
@@ -10,13 +15,22 @@ describe('ExtraAnnualAssemblyListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExtraAnnualAssemblyListComponent ],
+      declarations: [
+        ExtraAnnualAssemblyListComponent,
+        SearchFilterComponent,
+        SymbolSearchComponent,
+        TableComponent
+      ],
       imports: [
         HttpClientTestingModule,
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule,
+        NgxMaskDirective,
+        NgxMaskPipe
       ],
       providers: [
-        ExtraAnnualAssemblyService
+        ExtraAnnualAssemblyService,
+        provideNgxMask()
       ]
     })
     .compileComponents();
